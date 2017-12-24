efcaef4567a065016244bb4deed4774a394f358a
John Lee <PiggySpeed@users.noreply.github.com>
2017-12-23 17:15:24 -0800

    configuration work

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+// import Routes from './config/routes.jsx';
+import Main from './pages/main';
+
+const renderComponent = (Component) => {
+  ReactDOM.render(
+  <AppContainer>
+    <Component />
+  </AppContainer>,
+  document.getElementById('content')
+);
+};
+
+renderComponent(Main);
+
+if (module.hot) {
+  module.hot.accept('./pages/main.js', () => {
+    const NextComponent = require('./pages/main.js').default;
+  renderComponent(NextComponent);
+})
+}
---
+++ webpack.config.babel.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const LAUNCH_COMMAND = process.env.NODE_ENV;
+
+const environmentPlugin = new webpack.DefinePlugin({
+  'process.env.NODE_ENV': JSON.stringify(LAUNCH_COMMAND)
+});
+
+const devConfig = {
+  devtool: 'source-map',
+  devServer: { hot: true },
+  plugins: [
+    environmentPlugin,
+    new webpack.NamedModulesPlugin(), // make it easier to see which dependencies are being patched
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
+
+const base = {
+  entry: [
+    'babel-polyfill',
+    'react-hot-loader/patch',
+    './src/index.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.jsx?$/, exclude: /node_modules/, use: ['babel-loader'] },
+      { test: /\.less$/, exclude: /node_modules/, use: ['style-loader', 'css-loader', 'less-loader'] }
+    ]
+  }
+};
+
+module.exports = Object.assign({}, base, devConfig);
